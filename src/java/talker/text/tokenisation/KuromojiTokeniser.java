dc60e7f1b549a561759f73c634677d075c6d38a9
Trejkaz (pen name) <trejkaz@trypticon.org>
2015-12-28 13:52:13 +1100

    One more punctuation types (that's the last one we weren't already checking.)

--- src/java/talker/text/tokenisation/KuromojiTokeniser.java
+++ src/java/talker/text/tokenisation/KuromojiTokeniser.java
@@ -41,33 +41,44 @@ public class KuromojiTokeniser {
 
     private Token convertToken(org.atilika.kuromoji.Token token) {
         String[] features = token.getAllFeaturesArray();
-        String pos = features[1];
+        String pos = features[0];
+        String subPos = features[1];
         String text = token.getSurfaceForm();
-        TokenType tokenType;
-        switch (pos) {
-            case "アルファベット":
-                // Workaround for these tokens having readings in the dictionary
-                // which we might want to substitute.
-                tokenType = TokenType.OTHER;
-                break;
-            case "句点":
-            case "読点":
-            case "括弧開":
-            case "括弧閉":
-                tokenType = TokenType.PUNCTUATION;
-                break;
-            case "空白":
-                tokenType = TokenType.WHITESPACE;
-                break;
-            default:
-                if (!wordPattern.matcher(text).find()) {
-                    // Workaround for things like "." coming back as "noun".
-                    tokenType = TokenType.PUNCTUATION;
-                } else if (token.getAllFeaturesArray().length > 7) {
-                    tokenType = TokenType.JAPANESE;
-                } else {
+        TokenType tokenType = null;
+
+        if ("記号".equals(pos)) {
+            switch (subPos) {
+                case "アルファベット":
+                    // Workaround for these tokens having readings in the dictionary
+                    // which we might want to substitute.
                     tokenType = TokenType.OTHER;
-                }
+                    break;
+                case "句点":
+                case "読点":
+                case "括弧開":
+                case "括弧閉":
+                case "一般":
+                    tokenType = TokenType.PUNCTUATION;
+                    break;
+                case "空白":
+                    tokenType = TokenType.WHITESPACE;
+                    break;
+                default:
+                    // Falls through to null check
+            }
+        }
+
+        if (tokenType == null) {
+            if (!wordPattern.matcher(text).find()) {
+                // Workaround for things like "." coming back as "noun".
+                tokenType = TokenType.PUNCTUATION;
+            } else if (token.getAllFeaturesArray().length > 7) {
+                // This basically assumes that if the dictionary knows the reading, it's Japanese.
+                // I don't know if they have slipped other non-Japanese stuff into the dictionary...
+                tokenType = TokenType.JAPANESE;
+            } else {
+                tokenType = TokenType.OTHER;
+            }
         }
 
         return new Token(text, tokenType);
